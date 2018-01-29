a38cb72bfac1413e5c77aab7b6d3f5f2bfe983dd
vk241994 <vk241994@gmail.com>
2018-01-29 15:51:35 +0700

    FoboZ
    Sibintek_practic

---
+++ TestProject_SibIntek/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestProject_SibIntek.Models;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace TestProject_SibIntek.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ICollection<IFormFile> files)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
+
